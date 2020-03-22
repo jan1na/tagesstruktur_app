05a60ea644f42a49568dc0e1ff1fce2d71b200ff
jan1na <antilope666@gmail.com>
2020-03-22 20:30:18 +0100

    added internal storage to save activities and skills

---
+++ app/src/main/java/com/example/tagesstruktur/Internal_Storage.java
@@ -0,0 +1,76 @@
+package com.example.tagesstruktur;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class Internal_Storage {
+
+    private String file;
+    private Context context;
+
+    public Internal_Storage(Context context, String file){
+        this.file = file;
+        this.context = context;
+    }
+
+    public void saveData(List<String> data) {
+        try {
+            // Open Stream to write file.
+            FileOutputStream out = context.openFileOutput(file, Context.MODE_APPEND);
+
+            for(String s: data){
+                s+="\n";
+                out.write(s.getBytes());
+            }
+            out.close();
+        } catch (Exception e) {
+            Toast.makeText(context,"Error in Internal Storage"+ e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void saveData(String data) {
+        try {
+            // Open Stream to write file.
+            FileOutputStream out = context.openFileOutput(file, Context.MODE_APPEND);
+            data+="\n";
+            out.write(data.getBytes());
+            out.close();
+        } catch (Exception e) {
+            Toast.makeText(context,"Error in Internal Storage"+ e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public List<String> readData() {
+        try {
+            // Open stream to read file.
+            FileInputStream in = context.openFileInput(file);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            List<String> data = new ArrayList<>();
+            String s = "";
+            while ((s = br.readLine()) != null) {
+                data.add(s);
+                s = "";
+            }
+            return data;
+
+        } catch (FileNotFoundException e){
+            return new ArrayList<>();
+
+        }catch (Exception e) {
+            Toast.makeText(context,"Error:"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/example/tagesstruktur/My_resources.java
+++ app/src/main/java/com/example/tagesstruktur/My_resources.java
@@ -18,32 +18,40 @@ import java.util.List;
 
 public class My_resources extends AppCompatActivity {
 
+    static boolean skillsIsClicked = true;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_resources);
 
+        final Internal_Storage skills_storage = new Internal_Storage(this, "skills");
+        final Internal_Storage activities_storage = new Internal_Storage(this, "activities");
 
 
-        final List<String> skills = new ArrayList<>();
+
+
+        final List<String> skills = skills_storage.readData();
+        /*
         skills.add("Fähigkeit 1");
         skills.add("Fähigkeit 2");
 
-        List<String>activities = new ArrayList<>();
-        activities.add("Aktivität 1");
-        activities.add("Aktivität 1");
+         */
 
-        LinearLayout skills_LL = findViewById(R.id.skills_LL);
+        List<String> activities = activities_storage.readData();
         /*
-
-        for(String s: skills){
-            TextView t = new TextView(this);
-            t.setText(s);
-            skills_LL.addView(t);
-
-        }
+        activities.add("Aktivität 1");
+        activities.add("Aktivität 1");
 
          */
+
+        LinearLayout skills_LL = findViewById(R.id.skills_LL);
+        addViewToList(skills_LL, skills);
+
+        LinearLayout activities_LL = findViewById(R.id.activities_LL);
+        addViewToList(activities_LL, activities);
+
+
 
         Button addSkill = findViewById(R.id.add_skill_B);
         addSkill.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +63,7 @@ public class My_resources extends AppCompatActivity {
                 res_SV.setAlpha(0.4f);
                 EditText add_ET = findViewById(R.id.add_resource_ET);
                 add_ET.setHint("Ressource");
+                My_resources.skillsIsClicked = true;
             }
         });
 
@@ -63,13 +72,35 @@ public class My_resources extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText add_ET = findViewById(R.id.add_resource_ET);
-                skills.add(add_ET.getText().toString());
+                if(My_resources.skillsIsClicked) {
+                    skills_storage.saveData(add_ET.getText().toString());
+                    LinearLayout skills_LL = findViewById(R.id.skills_LL);
+                    addViewToList(skills_LL, add_ET.getText().toString());
+
+                }else{
+                    activities_storage.saveData(add_ET.getText().toString());
+                    LinearLayout activities_LL = findViewById(R.id.activities_LL);
+                    addViewToList(activities_LL, add_ET.getText().toString());
+                }
                 RelativeLayout add_resource_LL = findViewById(R.id.add_resource_RL);
                 add_resource_LL.setVisibility(View.INVISIBLE);
                 ScrollView res_SV = findViewById(R.id.ressources_SV);
                 res_SV.setAlpha(1.0f);
-                finish();
-                startActivity(getIntent());
+            }
+        });
+
+        Button add_activity_B = findViewById(R.id.add_activity_B);
+
+        add_activity_B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout add_resource_LL = findViewById(R.id.add_resource_RL);
+                add_resource_LL.setVisibility(View.VISIBLE);
+                ScrollView res_SV = findViewById(R.id.ressources_SV);
+                res_SV.setAlpha(0.4f);
+                EditText add_ET = findViewById(R.id.add_resource_ET);
+                add_ET.setHint("Aktivität");
+                My_resources.skillsIsClicked = false;
             }
         });
 
@@ -87,6 +118,29 @@ public class My_resources extends AppCompatActivity {
             return;
         }
         super.onBackPressed();
+    }
+
+    private void addViewToList(LinearLayout linearLayout, String data){
+        List<String> list = new ArrayList<>();
+        list.add(data);
+        addViewToList(linearLayout, list);
+    }
+
+    private void addViewToList(LinearLayout linearLayout, List<String> data){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(15,15,15,15);
+
+
+        for(String s: data){
+            TextView t = new TextView(this);
+            t.setText(s);
+            t.setTextSize(18);
+            t.setPadding(5,5,5,5);
+            t.setLayoutParams(params);
+            linearLayout.addView(t);
+
+        }
+
     }
 
 }
